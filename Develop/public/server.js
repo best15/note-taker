2f03891c547d5e63cc161f4fd096c7f7dac9287d
SBC <lochan.sharad@gmail.com>
2021-05-21 21:50:44 +0800

    server.js code updated to get request and response return to index file or notes file as well as able to read dbjson and return when requested.

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -4,19 +4,28 @@ const path = require('path');
 const app = express();
 const port = 8000;
 
+const fs = require('fs');
+const { response } = require('express');
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
-app.get('/*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'index.html'));
-
-})
 
 app.get('/notes', (request, response) => {
     response.sendFile(path.join(__dirname, 'notes.html'));
 
 })
 
-app.listen(port, () => console.log(`Server is listening at ${port} !!`));
+app.get('/api/notes', (request, response) => {
+
+    fs.readFile('../db/db.json', 'utf8', (error, data) => (error) ? console.log(error) : response.send(data));
+
+});
+
+app.get('/*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'index.html'));
+
+})
+
+
+app.listen(port, (err) => err ? console.log(err) : console.log(`Server is listening at ${port} !!`));
