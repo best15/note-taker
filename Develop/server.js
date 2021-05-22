b16d24488becd6641d42ae5265704d64f89e07c1
SBC <lochan.sharad@gmail.com>
2021-05-22 16:41:26 +0800

    npm node-id installed and each new note is appended with unique node id and store in dbjson. delete request function added to delete the selected note.

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const nodeid = require('node-id');
 
 const app = express();
 const port = 8000;
 
-const dbJson = require('./db/db.json')
+
 
 //static routes to public
 app.use(express.static('public'));
@@ -34,22 +35,43 @@ app.post('/api/notes', (request, response) => {
 
     const newNote = request.body;
 
+    //append new id in new Note
+    const id = nodeid(8);
+    newNote.id = id;
+
+    //store db.json file content in a variable and add new note in the variable
     fs.readFile('./db/db.json', 'utf8', (error, data) => {
         if (error) console.error(error);
         let dbJson = JSON.parse(data);
 
         dbJson.push(newNote);
-
+        //write dbJson contents in db.json file    
         fs.writeFile('./db/db.json', JSON.stringify(dbJson), (err) =>
             err ? console.error(err) : console.log('Success!'));
 
-    }
+    });
 
-    );
-
+    response.end();
 
 })
 
+app.delete('/api/notes/:id', (request, response) => {
+    const deleteId = request.params.id;
+    console.log(deleteId);
+    fs.readFile('./db/db.json', 'utf8', (error, data) => {
+        if (error) console.error(error);
+        let dbJson = JSON.parse(data);
 
+        dbJson.forEach(note => {
+            if (note.id == deleteId) {
+                const index = dbJson.indexOf(note);
+                dbJson.splice(index, 1);
+            }
+
+        });
+        console.log(dbJson);
+    });
+    response.end();
+});
 
 app.listen(port, (err) => err ? console.log(err) : console.log(`Server is listening at ${port} !!`));
